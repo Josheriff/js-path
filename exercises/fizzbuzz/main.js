072ba48e6b169aba8e696fe7361c06c704ada569
Jose <jakermater@gmail.com>
2018-02-21 08:34:03 +0100

    Some explanations on false
    - Some explanations
    - This will check not only it is different to 0, also different to (null, undefined,NaN,"",)  (all the explained)
    - Some people can argue that this code is less effective than != 0, and probaly it is in 0.00000000000000000000001 seconds
    - I forgot all the fizzbuzz rules but i think this aproach will work
    - Maybe a little pythonic but im 24x7 pythoning right now

--- exercises/fizzbuzz/main.js
+++ exercises/fizzbuzz/main.js
@@ -1,13 +1,34 @@
-var randomNumber = Math.round(Math.random() * 15);
+var randomNum = Math.round(Math.random() * 15);
 
 console.log(randomNumber);
+/*
+all of this values, are false and skip the if
+if (false)
+if (null)
+if (undefined)
+if (0)
+if (NaN)
+if ('')
+if ("")
+if (document.all) [1]
+*/
+function fizzBuzz(randomNumber){
+  if (randomNumber){
 
-if (randomNumber % 3 === 0 && randomNumber % 5 === 0 && randomNumber != 0) {
-  console.log('fizzbuzz');
-} else if (randomNumber % 3 === 0 && randomNumber != 0) {
-  console.log('fizz');
-} else if (randomNumber % 5 === 0 && randomNumber != 0) {
-  console.log('buzz');
-} else {
-  console.log('FAIL!');
+    if (randomNumber % 15 === 0 ) {
+      console.log('fizzbuzz');
+      return
+    } if (randomNumber % 3 === 0) {
+      console.log('fizz');
+      return
+    } if (randomNumber % 5 === 0) {
+      console.log('buzz');
+      return
+    } else {
+      console.log('FAIL!');
+      return
+    }
+  }  
 }
+
+fizzBuzz(randomNum);
